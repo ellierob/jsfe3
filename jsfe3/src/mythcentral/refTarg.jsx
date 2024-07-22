f308583b300ff379b83dddb177857855e31ff070
ellierob <elliotrobot15@gmail.com>
2024-07-22 12:28:01 +0600

    mythCental working

--- jsfe3/src/mythcentral/refTarg.jsx
+++ jsfe3/src/mythcentral/refTarg.jsx
@@ -1,4 +1,5 @@
 const refTarg = {
     "ref1": "taget text1",
     "ref2": "target text2",
+    "ref3": "target text3",
 }
--- jsfe3/src/mythcentral/txtLs.jsx
+++ jsfe3/src/mythcentral/txtLs.jsx
@@ -2,9 +2,9 @@
 const txtLs = [
     // "10_common_titans":
     {
-        "text": "10 common titans",
+        "text": "Nyx Zeus",
         "vars": {
-            "hsd": { "text": "10: cronus, rhea,", "ref": "ref1" },
+            "hmr": { "text": "Zeus afraid of nyx,", "ref": "ref1" },
             // "hmr": "",
             // "eudm": "",
             // "hrnm": "",
@@ -12,16 +12,27 @@ const txtLs = [
             // "rhps": "",
         },
     },
-    // "extra_titans":
     {
-        "text": "extra titans",
+        "text": "10 common titans",
         "vars": {
-            "hsd": { "text": "2: oceanus, tethys,", "ref": "ref2" },
+            "hsd": { "text": "10: cronus, rhea,", "ref": "ref2" },
             // "hmr": "",
             // "eudm": "",
             // "hrnm": "",
             // "apldr": "",
-            // "rhps": "",
+            // "rhps": { "text": "10: cronus, rhea,", "ref": "ref1" },
+        },
+    },
+    // "extra_titans":
+    {
+        "text": "extra titans",
+        "vars": {
+            "hsd": { "text": "2: oceanus, tethys,", "ref": "ref3" },
+            // "hmr": "",
+            // "eudm": "",
+            // "hrnm": "",
+            // "apldr": "",
+            "rhps": { "text": "4: oceanus, tethys, phorcys, dione", "ref": "ref3" },
         },
     },
 ];
